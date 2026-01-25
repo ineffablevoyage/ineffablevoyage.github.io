318f0b6163fb8a1de5a877394c8977a47133108f
sophiarika <sophiarika.zeleny@gmail.com>
2026-01-26 00:39:31 +0100

    Adding Daisy UI and start of designing more properly the website

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,12 @@
 	let { children } = $props();
 </script>
 
-<nav class="header" id="header_menu">
-        <h2>Menu</h2>
+<nav class="navbar bg-base-100 shadow-sm" id="header_menu">
+	<a class="btn btn-ghost text-xl" href="/">Menu</a>
+	<a class="btn btn-ghost text-xl" href="/ineffableVoyageZine">Ineffable Voyage zine</a>
+	<a class="btn btn-ghost text-xl" href="/ineffableCuisineZine">Ineffable Cuisine zine</a>
+	<a class="btn btn-ghost text-xl" href="/about">About</a>
+	<!-- <h2>Menu</h2> -->
 </nav>
 
 <svelte:head>
--- src/routes/ineffableVoyageZine/+page.svelte
+++ src/routes/ineffableVoyageZine/+page.svelte
@@ -1,5 +1,5 @@
 <div class="main_container">
     <div class="title">
-        <h1>Participants</h1>
+        <h1>Ineffable Voyage zine</h1>
     </div>
 </div>
