e79f2ddc13e0822169491f40e3c17c55b4338db5
sophiarika <sophiarika.zeleny@gmail.com>
2026-01-25 20:31:07 +0100

    Remade initial layout with svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import "$lib/zine_style.css";
 
 	let { children } = $props();
 </script>
+
+<nav class="header" id="header_menu">
+        <h2>Menu</h2>
+</nav>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
+
+
+<div class="main_container">
+    <div class="title">
+        <h1>Ineffable Voyage Fanzine</h1>
+    </div>
+    <div class="main_content">
+        <!-- <div class="zine_content">
+            <p>Welcome to the Ineffable Voyage Fanzine! Here you can explore various articles, stories, and illustrations that celebrate the wonders of our universe.</p>
+            <p>Enjoy your reading!</p>
+        </div> -->
+    </div>
+</div>
