8aa7f8c28f1329a8faf404fcf51291e4f73721e1
sophiarika <sophiarika.zeleny@gmail.com>
2026-01-26 13:00:07 +0100

    Centered navigation bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,19 @@
 </script>
 
 <nav class="navbar bg-base-100 shadow-sm" id="header_menu">
-	<a class="btn btn-ghost text-xl" href="/">Menu</a>
-	<a class="btn btn-ghost text-xl" href="/ineffableVoyageZine">Ineffable Voyage zine</a>
-	<a class="btn btn-ghost text-xl" href="/ineffableCuisineZine">Ineffable Cuisine zine</a>
-	<a class="btn btn-ghost text-xl" href="/about">About</a>
-	<!-- <h2>Menu</h2> -->
+	<div class="navbar-start">
+	</div>
+	<div class="navbar-center">
+		<a class="btn btn-ghost text-xl" href="/">Menu</a>
+		<a class="btn btn-ghost text-xl" href="/ineffableVoyageZine">Ineffable Voyage zine</a>
+		<a class="btn btn-ghost text-xl" href="/ineffableCuisineZine">Ineffable Cuisine zine</a>
+		<a class="btn btn-ghost text-xl" href="/about">About</a>
+	</div>
+	<div class="navbar-end">
+	</div>
 </nav>
+
+
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
