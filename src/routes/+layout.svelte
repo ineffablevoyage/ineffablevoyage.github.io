52a9b88f62137be2551279e2894edc280c982420
sophiarika <sophiarika.zeleny@gmail.com>
2026-01-27 00:13:35 +0100

    Started to work on the content of the pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,7 +20,7 @@
 	<div class="navbar-end">
 		<label class="swap swap-rotate">
 			<!-- this hidden checkbox controls the state -->
-			<input type="checkbox" class="theme-controller" value="zine_dark" />
+			<input type="checkbox" class="theme-controller" value="crowley" />
 
 			<!-- sun icon -->
 			<svg
@@ -40,56 +40,6 @@
 				d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
 			</svg>
 		</label>
-
-		<!-- <div class="dropdown mb-72">
-			<div tabindex="0" role="button" class="btn m-1">
-				Theme
-				<svg
-				width="12px"
-				height="12px"
-				class="inline-block h-2 w-2 fill-current opacity-60"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 2048 2048">
-				<path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
-				</svg>
-			</div>
-			<ul tabindex="-1" class="dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl">
-				<li>
-				<input
-					type="radio"
-					name="theme-dropdown"
-					class="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
-					aria-label="Light"
-					value="zine_light" />
-				</li>
-				<li>
-				<input
-					type="radio"
-					name="theme-dropdown"
-					class="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
-					aria-label="Dark"
-					value="zine_dark" />
-				</li>
-				<li>
-				<input
-					type="radio"
-					name="theme-dropdown"
-					class="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
-					aria-label="Aziraphale"
-					value="aziraphale" />
-				</li>
-				<li>
-				<input
-					type="radio"
-					name="theme-dropdown"
-					class="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
-					aria-label="Crowley"
-					value="Crowley" />
-				</li>
-			</ul>
-		</div> -->
-
-
 	</div>
 </nav>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,30 @@
-<!-- <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
-
-
-<div class="main_container">
+<!-- <div class="main_container">
     <div class="title">
         <h1>[Main page]</h1>
     </div>
     <div class="main_content">
-        <!-- <div class="zine_content">
-            <p>Welcome to the Ineffable Voyage Fanzine! Here you can explore various articles, stories, and illustrations that celebrate the wonders of our universe.</p>
-            <p>Enjoy your reading!</p>
-        </div> -->
+
+
+    </div>
+</div> -->
+
+<script lang="ts">
+	import background from '$lib/assets/images/ballon_goodomensfanzine.jpg';
+
+</script>
+
+<div
+  class="hero min-h-screen"
+  style="background-image: url('{background}');"
+>
+    <div class="hero-overlay"></div>
+    <div class="hero-content text-neutral-content text-center">
+        <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">Ineffable Cuisine</h1>
+            <p class="mb-5">
+                A new fanzine made by the people that made Ineffable voyage. 
+            </p>
+            <!-- <button class="btn btn-primary">Get Started</button> -->
+        </div>
     </div>
 </div>
