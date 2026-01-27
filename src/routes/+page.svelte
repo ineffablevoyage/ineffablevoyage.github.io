8bd7dfc0bfbf15095d71bd37be11a53e51015a89
sophiarika <sophiarika.zeleny@gmail.com>
2026-01-28 00:06:32 +0100

    Working on the main page of the website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,8 @@
 </div> -->
 
 <script lang="ts">
-	import background from '$lib/assets/images/ballon_goodomensfanzine.jpg';
+	import background from '$lib/assets/images/ballon_goodomensfanzine_field.jpg';
+    import zine_image from '$lib/assets/images/ballon_goodomensfanzine.jpg';
 
 </script>
 
@@ -17,14 +18,45 @@
   class="hero min-h-screen"
   style="background-image: url('{background}');"
 >
-    <div class="hero-overlay"></div>
+    <!-- <div class="hero-overlay"></div> -->
     <div class="hero-content text-neutral-content text-center">
-        <div class="max-w-md">
-            <h1 class="mb-5 text-5xl font-bold">Ineffable Cuisine</h1>
+        <div class="max-w-xl">
+            <div class="card card-side bg-base-100 shadow-sm">
+                <div class="card-body">
+                    <h2 class="card-title">Ineffable Cuisine soon</h2>
+                    <p>Comming soon.</p>
+                    <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Preorder soon</button>
+                    </div>
+                </div>
+                <figure>
+                    <img
+                    src="{zine_image}"
+                    alt="fanzine cover" />
+                </figure>
+            </div>
+            <!-- <h1 class="mb-5 text-5xl font-bold">Ineffable Cuisine</h1>
             <p class="mb-5">
                 A new fanzine made by the people that made Ineffable voyage. 
             </p>
-            <!-- <button class="btn btn-primary">Get Started</button> -->
+            <button class="btn btn-primary">Preorder soon !</button> -->
         </div>
     </div>
 </div>
+
+<!-- <div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+        <img
+        src="{zine_image}"
+        class="max-w-sm rounded-lg shadow-2xl"
+        alt=""
+        />
+        <div>
+            <h1 class="text-5xl font-bold">Ineffable Cuisine Fanzine </h1>
+            <p class="py-6">
+                A new fanzine following the Ineffable Voyage one made by the same group ! New arts, new fics, and even more sweet !
+            </p>
+            <button class="btn btn-primary">Preorder soon !</button>
+        </div>
+    </div>
+</div> -->
