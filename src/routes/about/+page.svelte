2d47e34eae6b9677d02f305bbee95d367f7f9600
sophiarika <sophiarika.zeleny@gmail.com>
2026-03-03 20:02:32 +0100

    Working on about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,21 @@
-<div class="main_container">
-    <div class="title">
-        <h1>About</h1>
+<!-- About -->
+ <script lang="ts">
+    import { resolve } from '$app/paths';
+ </script>
+
+<div class="flex justify-center">
+    <div class="card-xl card-dash bg-base-100 w-3/4">
+        <div class="card-body items-center text-center">
+            <h2 class="card-title">About</h2>
+            <p>The idea of the first fanzine was born from the collective mind of our French-speaking server, "De Bonnes Prédictions". The "what if" that started it all presented as so : "What if our dear ineffable husbands decided to visit France? And, since some of us are Belgians, they could probably make a little detour in the country of the best fries and pralines?"</p>
+            <p>The project was on, and we enthusiastically got to work.</p>
+            <br>
+            <br>
+            <p>Learn more about our projects behiind !</p>
+            <div class="card-actions justify-end">
+                <a class="btn btn-primary" href="{resolve("/ineffableVoyageZine")}">Ineffable Voyage</a>
+                <a class="btn btn-primary" href="{resolve("/ineffableCuisineZine")}">Ineffable Cusine</a>
+            </div>
+        </div>
     </div>
 </div>
-
-<!-- TODO : https://daisyui.com/components/card/  =>  To use to create for participants -->
