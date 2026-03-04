ffa39ed80e8a4bef2a9681997c7603f598580bc9
sophiarika <sophiarika.zeleny@gmail.com>
2026-03-04 22:34:34 +0100

    Made navebar collapse into the side on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,8 @@
 	let { children } = $props();
 </script>
 
-<nav class="navbar bg-base-300 shadow-sm" id="header_menu">
+
+<!-- <nav class="navbar bg-base-300 shadow-sm" id="header_menu">
 	<div class="navbar-start">
 	</div>
 	<div class="navbar-center">
@@ -20,13 +21,70 @@
 		<a class="btn btn-ghost text-xl" href="{resolve("/about")}">About</a>
 	</div>
 	<div class="navbar-end">
-		<label class="swap swap-rotate text-4xl">
+		<label class="swap swap-rotate text-4xl"> -->
 			<!-- this hidden checkbox controls the state -->
-			<input type="checkbox" class="theme-controller" value="crowley" />
+			<!-- <input type="checkbox" class="theme-controller" value="crowley" />
 
 			<div class="swap-on">😈</div>
 			<div class="swap-off">😇</div>
 		</label>
+	</div>
+</nav> -->
+
+
+
+<nav class="drawer">
+	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col">
+		<!-- Navbar -->
+		<div class="navbar bg-base-300 shadow-sm w-full" id="header_menu">
+			<div class="flex-none lg:hidden">
+				<label for="my-drawer-2" aria-label="open sidebar" class="btn btn-square btn-ghost">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="inline-block h-6 w-6 stroke-current"
+				>
+					<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M4 6h16M4 12h16M4 18h16"
+					></path>
+				</svg>
+				</label>
+			</div>
+			<div class="navbar-start"></div>
+			<div class="navbar-center hidden flex-none lg:block">
+				<ul class="menu menu-horizontal">
+				<!-- Navbar menu content here -->
+					<a class="btn btn-ghost text-xl" href="{resolve("/")}">Home</a>
+					<a class="btn btn-ghost text-xl" href="{resolve("/ineffableVoyageZine")}">Ineffable Voyage zine</a>
+					<a class="btn btn-ghost text-xl" href="{resolve("/ineffableCuisineZine")}">Ineffable Cuisine zine</a>
+					<a class="btn btn-ghost text-xl" href="{resolve("/about")}">About</a>
+				</ul>
+			</div>
+			<div class="navbar-end">
+				<label class="swap swap-rotate text-4xl">
+					<!-- this hidden checkbox controls the state -->
+					<input type="checkbox" class="theme-controller" value="crowley" />
+					<div class="swap-on">😈</div>
+					<div class="swap-off">😇</div>
+				</label>
+			</div>
+		</div>
+
+	</div>
+	<div class="drawer-side">
+		<label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+		<ul class="menu bg-base-200 min-h-full w-80 p-4">
+			<!-- Sidebar content here -->
+			<a class="btn btn-ghost text-xl" href="{resolve("/")}">Home</a>
+			<a class="btn btn-ghost text-xl" href="{resolve("/ineffableVoyageZine")}">Ineffable Voyage zine</a>
+			<a class="btn btn-ghost text-xl" href="{resolve("/ineffableCuisineZine")}">Ineffable Cuisine zine</a>
+			<a class="btn btn-ghost text-xl" href="{resolve("/about")}">About</a>
+		</ul>
 	</div>
 </nav>
 
